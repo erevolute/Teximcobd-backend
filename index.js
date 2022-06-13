ddff4bb53df175baad4e8dbe6c644c4ffe1a3b08
Ahad hossain <ahadxx99@gmail.com>
2022-06-13 15:39:08 -0500

    contact-collection

--- index.js
+++ index.js
@@ -4,19 +4,59 @@ const cors = require('cors');
 const port = process.env.PORT || 5000 ;
 const { MongoClient, ServerApiVersion } = require('mongodb');
 require('dotenv').config()
+const ObjectId = require('mongodb').ObjectId;
+const fileUpload = require('express-fileupload')
+
+const nodemailer = require('nodemailer');
+const smtpTransport = require('nodemailer-smtp-transport');
 
 app.use(cors());
 app.use(express.json())
+app.use(fileUpload())
 
 const uri = `mongodb+srv://${process.env.USER_NAME}:${process.env.USER_PASS}@cluster0.is333.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
 
-console.log(uri)
+
+
+// const emailSenderOptions = {
+//     auth: {
+//       api_key: process.env.EMAIL_KEY
+//     }
+//   }
+  
+// const emailClient = nodemailer.createTransport(smtpTransport(emailSenderOptions));
+  
+
+// const sendEmail = ({email , name , message})=>{
+
+//     const mailOptions = {
+//         from: email ,
+//         to: 'ahadxx99@gmail.com',
+//         subject: `${name} Sending Email using Node.js[nodemailer]`,
+//         text: message,
+//         html : ''
+//       };
+
+//         console.log("aa", email, name , message)
+
+//       emailClient.sendMail(mailOptions, function(error, info){
+//         if (error) {
+//           console.log(error);
+//         } else {
+//           console.log('Email sent: ' + info.response);
+//         }
+//       }); 
+
+// }
+    
+
 async function run() {
     try {
       await client.connect();
       const portfolioCollection = client.db("eRevolute").collection('portfolioCollection');
       const blogsCollection = client.db("eRevolute").collection('blogsCollection');
+      const contactCollection = client.db("eRevolute").collection('contactCollection');
 
       app.get('/portfolio' , async(req, res)=>{
         const query = {}
@@ -29,7 +69,39 @@ async function run() {
         const result = await blogsCollection.find(query).toArray()
         res.send(result)
      })
-  
+
+     app.post('/contact-collection', async(req,res)=>{
+        const email = req.body.email;
+        const name = req.body.name;
+        const message = req.body.message;
+        const image = req.files.img;
+        const imageData = image.data;
+        const encodedImage = imageData.toString('base64')
+        const imgBuffer = Buffer.from(encodedImage , 'base64')
+
+        const query = {
+            email : email,
+            name : name ,
+            message : message,
+            image : imgBuffer
+        }
+        // sendEmail(query)
+        const result = await contactCollection.insertOne(query)
+        res.send(result)
+
+     })
+     app.delete('/contact-collection/:id' , async(req, res)=>{
+        const id = req.params.id;
+        const query = {_id:ObjectId(id)}
+        const result = await contactCollection.deleteOne(query)
+        res.send(result)
+     })
+
+     app.get('/contact-collection' , async(req, res)=>{
+        const query = {}
+        const result = await contactCollection.find(query).toArray()
+        res.send(result)
+     })
     } finally {
         //  await client.close();
 
